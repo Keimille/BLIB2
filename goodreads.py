c5be0839d822a89d3beca08124dc002b87c5175a
Keith Miller <keithemmanuel@gmail.com>
2020-11-26 15:19:58 -0500

    Adding features using Goodreads API

--- goodreads.py
+++ goodreads.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import caching
+import cgi
 
 #import API key
 goodreadsKey = os.environ.get('VARENV')
@@ -9,7 +9,7 @@ def main():
     search_goodreads()
 def search_goodreads():
     #search by Title, author or ISBN
-    search = input("Enter search: ")
+    search = iformData.getvalue('search')
 
     url = str("https://goodreads.com/search/index.html?key=" + goodreadsKey + '&q=' + search)
 
@@ -19,4 +19,16 @@ def search_goodreads():
     #return response
     print(response.text)
     print(url)
+
+    print """ Content-type: text/html\n\n
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Test</title>
+        </head>
+        <body>
+            <p> Books {0} </p>
+        </body>
+    </html>
+    """ .format(response.txt)
 main()
